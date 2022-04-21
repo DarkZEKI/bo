aa6b0ec35044913142ed513761c117a487b5cf3a
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2022-04-21 14:11:19 -0500

    Update spamlink.js

--- plugins/spamlink.js
+++ plugins/spamlink.js
@@ -32,7 +32,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 let _vote = conn.prepareMessageFromContent(target, {
 "listMessage":  {
 "title": "\t\t\t*‧ 🐋 Auto spam Bot 🐋 ‧*",
-"description": `\nHemos detectado su enlace en uno de nuestros grupo por lo tanto se enviará spam automáticamente.\n\n*Grupo nro 1*\nhttps://chat.whatsapp.com/EphX7iaMsKj70m0BrZsmvw\n\n*Grupo nro 2*\nhttps://chat.whatsapp.com/FVWUefIddjH5czTfujL2NA`,
+"description": `\nHemos detectado su enlace en uno de nuestros grupo por lo tanto se enviará spam automáticamente.\n\n*Grupo nro 1*\nhttps://chat.whatsapp.com/EphX7iaMsKj70m0BrZsmvw\n\n*Grupo nro 2*\nhttps://chat.whatsapp.com/EphX7iaMsKj70m0BrZsmvw`,
 "buttonText": "Opciones",
 "listType": "SINGLE_SELECT",
 "sections": [
@@ -43,7 +43,7 @@ let _vote = conn.prepareMessageFromContent(target, {
 "rowId": 'lolibot'
 },
 {
-"title": '7w7\n\n*Grupo nro 1*\nhttps://chat.whatsapp.com/EphX7iaMsKj70m0BrZsmvw\n\n*Grupo nro 2*\nhttps://chat.whatsapp.com/FVWUefIddjH5czTfujL2NA',
+"title": '7w7\n\n*Grupo nro 1*\nhttps://chat.whatsapp.com/EphX7iaMsKj70m0BrZsmvw\n\n*Grupo nro 2*\nhttps://chat.whatsapp.com/EphX7iaMsKj70m0BrZsmvw',
 "rowId": 'lolibot'
 }
 ]}]}}, { quoted: faker, contextInfo: { forwardingScore:999, isForwarded:true, mentionedJid: member } })
