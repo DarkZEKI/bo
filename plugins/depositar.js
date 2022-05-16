993f176db4d787433a5b94c68ae05edf8ad36695
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2022-05-15 21:02:09 -0500

    Update depositar.js

--- plugins/depositar.js
+++ plugins/depositar.js
@@ -1,26 +1,14 @@
 let { MessageType } = require('@adiwajshing/baileys')
-let pajak = 0.02
-let handler = async (m, { conn, text }) => {
-  if (!text) throw 'Ingrese la cantidad de exp que se dará'
-  let who
-  if (m.isGroup) who = m.mentionedJid[0]
-  else who = m.chat
-  if (!who) throw 'Etiqueta a uno'
-  let txt = text.replace('@' + who.split`@`[0], '').trim()
-  if (isNaN(txt)) throw 'Solo números'
-  let xp = parseInt(txt)
-  let exp = xp
-  let pjk = Math.ceil(xp * pajak)
-  exp += pjk
-  if (exp < 1) throw 'Minimo 1'
-  let users = global.DATABASE._data.users
-  if (exp > users[m.sender].exp) throw 'Exp insuficiente para transferir'
-  users[m.sender].exp -= exp
-  users[who].exp += xp
+
+global.DATABASE._data.users[m.quoted.sender].money += 50000
+
+global.DATABASE._data.users[m.sender].money -= 50000
+
+m.quoted.reply ('haz recibido 50k')
 
 }
 handler.help = ['pay @user <amount>']
-handler.tags = ['xp']
+handler.tags = ['menu']
 handler.command = /^pay$/
 handler.rowner = true
 
